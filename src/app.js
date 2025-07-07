feecbf7eafb454bdebd07f023fd263e2943964c9
Abhishek Pandey <abhi.ecc.cs@gmail.com>
2025-07-07 17:23:25 +0530

    createlogin API

--- src/app.js
+++ src/app.js
@@ -2,83 +2,104 @@ const express = require('express');
 const connectDB = require('./config/database')
 const app = express();
 const User = require('./models/user')
-const {validateSignUpData} = require('./utils/validation') 
+const { validateSignUpData } = require('./utils/validation')
 const bcrypt = require('bcrypt');
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    try{
-    validateSignUpData(req)
-    const {firstName, lastName, emailId, password} = req.body;
-    const passwordHash = await bcrypt.hash(password, 10);
-    const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash
-    });
-    await user.save();
-    res.send("User added successfully!");
+    try {
+        validateSignUpData(req)
+        const { firstName, lastName, emailId, password } = req.body;
+        const passwordHash = await bcrypt.hash(password, 10);
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
+        await user.save();
+        res.send("User added successfully!");
     }
-    catch(err){
+    catch (err) {
         res.status(400).send("ERROR :" + err.message)
     }
 })
 
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
-    try{
-        const getUser = await User.findOne({emailId:userEmail});
-        if(getUser.length === 0){
+    try {
+        const getUser = await User.findOne({ emailId: userEmail });
+        if (getUser.length === 0) {
             res.status(404).send("User not found")
-        }else{
-        res.send(getUser)
+        } else {
+            res.send(getUser)
         }
     }
-    catch(err){
+    catch (err) {
         res.status(404).send("Something went wrong!!")
     }
 })
+//Login API
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId })
+        if (!user) {
+            throw new Error("Invalid credentials ")
+        }
 
-app.delete('/user', async(req, res) => {
+        const isPassworsValid = await bcrypt.compare(password, user.password);
+        if (isPassworsValid) {
+            res.send("Login successfull!!")
+        } else {
+            throw new Error("Invalid credential")
+        }
+
+    }
+    catch (err) {
+        res.status(404).send("ERROR :" + err.message)
+    }
+})
+
+app.delete('/user', async (req, res) => {
     const userId = req.body.userId
-    try{
+    try {
         const user = await User.findByIdAndDelete(userId);
         res.send("User deleted successfully")
     }
-    catch(err){
+    catch (err) {
         res.status(404
 
         ).send("Something wend wrong")
     }
 })
 // need to update document using user email id
-app.patch("/user/:userId", async(req, res) => {
+app.patch("/user/:userId", async (req, res) => {
     const userId = req.params?.userId
     const data = req.body
-    try{
-        const ALLOWED_UPDATE = ["gender", "age", "skill", "photoUrl", "about", "password" ]
+    try {
+        const ALLOWED_UPDATE = ["gender", "age", "skill", "photoUrl", "about", "password"]
         const isUpdateAllow = Object.keys(data).every((k) => ALLOWED_UPDATE.includes(k))
-        if(!isUpdateAllow){
+        if (!isUpdateAllow) {
             throw new Error("Update not allow")
         }
-        if(data.skill.length > 10){
+        if (data.skill.length > 10) {
             throw new Error("Skill cannot be more than 10")
         }
-       const user = await User.findByIdAndUpdate({_id:userId}, data,{retutnDocument: 'before', runValidators: true} )
-       console.log(user)
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { retutnDocument: 'before', runValidators: true })
+        console.log(user)
         res.send("User upadate successfully " + user)
-    }catch(err){
+    } catch (err) {
         res.status(404).send("Update failed " + err.message)
     }
 })
 
 
-app.get("/feed", async(req, res) => {
-    try{
+app.get("/feed", async (req, res) => {
+    try {
         const user = await User.find({})
         res.send(user)
-    }catch(err){
+    } catch (err) {
         res.status(404).send("Something went wrong")
     }
 })
